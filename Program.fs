13f2d2d75b06c98787f1783dc85662f40d759f29
Anu Bandi <ultimateanu@gmail.com>
2020-11-20 18:25:19 -0800

    added union type to represent directory structure with hashes

--- Program.fs
+++ Program.fs
@@ -1,11 +1,11 @@
 open System
-open System.IO;
+open System.IO
 open System.Security.Cryptography
 open System.Text
 
 
 let computeHashString (input : string) =
-    input 
+    input
         |> Encoding.ASCII.GetBytes
         |> SHA256.Create().ComputeHash
         |> Seq.map (fun c -> c.ToString("x2"))
@@ -13,22 +13,54 @@ let computeHashString (input : string) =
 
 
 let computeHashStringFromFile filePath =
-    if not (File.Exists filePath) then
-        None
-    else
-        use file = File.OpenRead filePath
-        file
-            |> SHA256.Create().ComputeHash
-            |> Seq.map (fun c -> c.ToString("x2"))
+    assert File.Exists filePath
+    use file = File.OpenRead filePath
+    file
+        |> SHA256.Create().ComputeHash
+        |> Seq.map (fun c -> c.ToString("x2"))
+        |> Seq.reduce (+)
+
+
+type ItemHash =
+    | File of path : string * hash : string
+    | Dir of path : string * hash : string * children : seq<ItemHash>
+
+
+let getHash itemHash =
+    match itemHash with
+        | File (_, hash) ->
+            hash
+        | Dir (_, hash, _) ->
+            hash
+
+
+let rec makeDirHashStructure dirPath =
+    assert Directory.Exists(dirPath)
+    let children =
+        dirPath
+            |> Directory.EnumerateFileSystemEntries
+            |> Seq.choose makeHashStructure
+    let childrenHash =
+        children
+            |> Seq.map getHash
             |> Seq.reduce (+)
-            |> Some
-    
+            |> computeHashString
+    Dir(path = dirPath, hash = childrenHash, children = children)
+
+
+and makeHashStructure path =
+    if File.Exists(path) then
+        Some(File(path = path, hash = (computeHashStringFromFile path)))
+    else if Directory.Exists(path) then
+        Some(makeDirHashStructure path)
+    else
+        None
+
 
 [<EntryPoint>]
 let main argv =
     for arg in argv do
-        match (computeHashStringFromFile arg) with
-            | Some(hash) -> printfn "%s %s" hash arg
-            | None -> ()
+        let hashStructure = makeHashStructure arg
+        printfn "%A" hashStructure
 
     0
