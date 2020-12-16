8566c0070a734e9da4c037cfba3107d43e1d4a1c
Anu Bandi <ultimateanu@gmail.com>
2020-12-16 13:30:12 -0800

    moved core logic to library and added unit test

--- src/HashUtil.Tests/Tests.fs
+++ src/HashUtil.Tests/Tests.fs
@@ -1,14 +1,28 @@
-module Tests
-
 open System
 open Xunit
-open HashUtil
+open Xunit.Abstractions
+open System.IO
+open HashUtil.FS
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+type FileSystem(output:ITestOutputHelper) = 
+    let tempDir = Path.GetFullPath(Path.Combine(Path.GetTempPath(), "hashdir_test_" + Guid.NewGuid().ToString()))
+    let rootDir = Path.Combine(tempDir, "root")
+    do
+        Directory.CreateDirectory(tempDir) |> ignore;
+        output.WriteLine("Created " + tempDir)
+        Directory.CreateDirectory(rootDir) |> ignore;
+        File.WriteAllText(Path.Combine(rootDir, "shakespeare.txt"), "To be or not to be...");
 
-[<Fact>]
-let ``Bigger`` () =
-    let ans = Say.bigger 3
-    Assert.Equal(6, ans)
+
+    [<Fact>]
+    let ``Check root dir hash`` () =
+        let optHashStructure = makeHashStructure false rootDir
+        Assert.True(optHashStructure.IsSome)
+        let dirHash = getHash optHashStructure.Value
+        Assert.Equal("9da9449853c7cfafb7b428097cbb3aaf45587b66146bf01a1515c667f1e24707", dirHash)
+
+
+    interface IDisposable with
+        member x.Dispose() =
+            Directory.Delete(tempDir, true)
+            output.WriteLine("Deleted " + tempDir)
--- src/HashUtil/Library.fs
+++ src/HashUtil/Library.fs
@@ -2,11 +2,8 @@
 
 open System.Security.Cryptography
 open System.Text
+open System.IO
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
-    let bigger x = 2*x
 
 module Checksum =
     let computeHashString (input : string) =
@@ -15,3 +12,79 @@ module Checksum =
             |> SHA256.Create().ComputeHash
             |> Seq.map (fun c -> c.ToString("x2"))
             |> Seq.reduce (+)
+
+    let computeHashStringFromFile filePath =
+        assert File.Exists filePath
+        use file = File.OpenRead filePath
+        file
+            |> SHA256.Create().ComputeHash
+            |> Seq.map (fun c -> c.ToString("x2"))
+            |> Seq.reduce (+)
+
+
+module FS =
+    type ItemHash =
+        | File of path : string * hash : string
+        | Dir of path : string * hash : string * children : seq<ItemHash>
+
+    let getHash itemHash =
+        match itemHash with
+            | File (_, hash) ->
+                hash
+            | Dir (_, hash, _) ->
+                hash
+
+    let rec makeDirHashStructure includeHiddenFiles dirPath =
+        assert Directory.Exists(dirPath)
+        let children =
+            dirPath
+                |> Directory.EnumerateFileSystemEntries
+                |> Seq.sortBy(id)
+                |> Seq.choose (makeHashStructure includeHiddenFiles)
+        let childrenHash =
+            children
+                |> Seq.map getHash
+                |> Seq.reduce (+)
+                |> Checksum.computeHashString
+        Dir(path = dirPath, hash = childrenHash, children = children)
+    
+    and makeHashStructure includeHiddenFiles path =
+        if File.Exists(path) then
+            if ((not includeHiddenFiles) &&
+                (File.GetAttributes(path) &&& FileAttributes.Hidden).Equals(FileAttributes.Hidden)) then
+                None
+            else
+                Some(File(path = path, hash = (Checksum.computeHashStringFromFile path)))
+        else if Directory.Exists(path) then
+            Some(makeDirHashStructure includeHiddenFiles path)
+        else
+            None
+    
+    let makeLeftSpacer levels =
+        match levels with
+            | [] -> ""
+            | lastLevelActive :: parentsActive ->
+                let parentSpacer =
+                    parentsActive
+                        |> List.rev
+                        |> List.map (fun isActive -> if isActive then "│   " else "    ")
+                        |> System.String.Concat
+    
+                let curSpacer = if lastLevelActive then "├── " else "└── "
+                parentSpacer + curSpacer
+    
+    let rec printHashStructureHelper structure (levels:List<bool>) =
+        match structure with
+            | File (path, hash) ->
+                printfn "%s%s %s" (makeLeftSpacer levels) hash (Path.GetFileName path)
+            | Dir (path, hash, children) ->
+                printfn "%s%s %c%s" (makeLeftSpacer levels) hash
+                    Path.DirectorySeparatorChar (DirectoryInfo(path).Name)
+                let allButLastChild = Seq.take (Seq.length children - 1) children
+                let lastChild = Seq.last children
+                for child in allButLastChild do
+                    printHashStructureHelper child (true :: levels)
+                printHashStructureHelper lastChild (false :: levels)
+
+    let rec printHashStructure structure =
+        printHashStructureHelper structure []
