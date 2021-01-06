4a71b4d623e9d478696206fdb953f46b3a170e67
Anu Bandi <ultimateanu@gmail.com>
2021-01-05 18:54:30 -0800

    removing extra line in stdout

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -3,29 +3,36 @@ open HashUtil.FS
 open System.IO
 
 
-type Options = {
-  [<Option('i', "include-hidden-files", Default = false, HelpText = "Include hidden files.")>] IncludeHiddenFiles : bool;
-  [<Option('e', "include-empty-dir", Default = true, HelpText = "Include empty directories.")>] IncludeEmptyDir : bool;
-  [<Value(0, Required = true, MetaName="input", HelpText = "Input directories or files.")>] Input : seq<string>;
-}
+type Options =
+    { [<Option('i', "include-hidden-files", Default = false, HelpText = "Include hidden files.")>]
+      IncludeHiddenFiles: bool
+      [<Option('e', "include-empty-dir", Default = true, HelpText = "Include empty directories.")>]
+      IncludeEmptyDir: bool
+      [<Value(0, Required = true, MetaName = "input", HelpText = "Input directories or files.")>]
+      Input: seq<string> }
 
 
-let run (o : Options) =
+let run (o: Options) =
     for item in o.Input do
-        let optHashStructure = makeHashStructure o.IncludeHiddenFiles o.IncludeEmptyDir item
+        let optHashStructure =
+            makeHashStructure o.IncludeHiddenFiles o.IncludeEmptyDir item
+
         let strWriter = new StringWriter()
+
         match optHashStructure with
-            | Error e -> printfn "Error: %s" e
-            | Ok hashStructure ->
-                printHashStructure hashStructure strWriter
-                printfn "%s" (strWriter.ToString())
+        | Error e -> printfn "Error: %s" e
+        | Ok hashStructure ->
+            printHashStructure hashStructure strWriter
+            printf "%s" (strWriter.ToString())
 
 
 [<EntryPoint>]
 let main argv =
-    let parsedResult = CommandLine.Parser.Default.ParseArguments<Options>(argv)
+    let parsedResult =
+        CommandLine.Parser.Default.ParseArguments<Options>(argv)
+
     match parsedResult with
-        | :? Parsed<Options> as parsed -> run parsed.Value
-        | _ -> ()
+    | :? (Parsed<Options>) as parsed -> run parsed.Value
+    | _ -> ()
 
     0
