864295ff32be0008cf1f61deb7ab6bd91b882c1e
Anu Bandi <ultimateanu@gmail.com>
2021-01-15 12:44:54 -0800

    updating release script to use rid

--- src/make_release.fsx
+++ src/make_release.fsx
@@ -31,53 +31,93 @@ type Architecture =
     | Arm64
     | Any
 
+type RuntimeIdentifier =
+    // MacOS
+    | Mac64
+    // Windows
+    | Win86
+    | Win64
+    | WinArm
+    | WinArm64
+    // Linux
+    | Linux64
+    | LinuxArm
+    | LinuxArm64
+
 type PublishSpec =
     { Name: string
+      Rid: RuntimeIdentifier
       Os: OperatingSystem
       Architecture: Architecture
       Compression: Compression }
 
 let macProfiles =
     [ { Name = "mac64"
+        Rid = Mac64
         Os = MacOS
         Architecture = X64
         Compression = TarGz } ]
 
 let windowsProfiles =
     [ { Name = "win32"
+        Rid = Win86
         Os = Windows
         Architecture = X86
         Compression = Zip }
       { Name = "win64"
+        Rid = Win64
         Os = Windows
         Architecture = X64
         Compression = Zip }
       { Name = "win32arm"
+        Rid = WinArm
         Os = Windows
         Architecture = Arm
         Compression = Zip }
       { Name = "win64arm"
+        Rid = WinArm64
         Os = Windows
         Architecture = Arm64
         Compression = Zip } ]
 
 let linuxProfiles =
-    [ { Name = "linux32"
-        Os = Linux
-        Architecture = X86
-        Compression = TarGz }
-      { Name = "linux64"
+    [ { Name = "Linux64"
+        Rid = Linux64
         Os = Linux
         Architecture = X64
+        Compression = TarGz }
+      { Name = "LinuxArm"
+        Rid = LinuxArm
+        Os = Linux
+        Architecture = Arm
+        Compression = TarGz }
+      { Name = "LinuxArm64"
+        Rid = LinuxArm64
+        Os = Linux
+        Architecture = Arm64
         Compression = TarGz } ]
 
 // TODO: add zip also?
 let dotNetProfiles =
     [ { Name = "dotnet"
+        Rid = Win86
         Os = OperatingSystem.Any
         Architecture = Any
         Compression = TarGz } ]
 
+let RuntimeIdentifierString id =
+    match id with
+    // MacOS
+    | Mac64 -> "osx-x64"
+    // Windows
+    | Win86 -> "win-x86"
+    | Win64 -> "win-x64"
+    | WinArm -> "win-arm"
+    | WinArm64 -> "win-arm64"
+    // Linux
+    | Linux64 -> "linux-x64"
+    | LinuxArm -> "linux-arm"
+    | LinuxArm64 -> "linux-arm64"
 
 let displayOsString os =
     match os with
@@ -114,14 +154,14 @@ let dotnet args = runProcess "dotnet" args
 
 let buildBinary (profile: PublishSpec) =
     // Build binary
-    dotnet (sprintf "publish -c Release -p:PublishProfile=%s src/App/App.fsproj" profile.Name)
+    dotnet (sprintf "publish -c Release -p:PublishProfile=binary -p:RuntimeIdentifier=%s src/App/App.fsproj"
+        (RuntimeIdentifierString profile.Rid))
 
     // Create published dir
     let releaseName =
         sprintf "hashdir_%s_%s_%s" versionStr (displayOsString profile.Os) (displayArchString profile.Architecture)
 
-    let oldProfileDir =
-        Path.Combine("src/App/bin/Release/net5.0/publish", profile.Name)
+    let oldProfileDir = "src/App/bin/Release/net5.0/publish/binary"
 
     let newProfileDir = Path.Combine(outputDir, releaseName)
 
