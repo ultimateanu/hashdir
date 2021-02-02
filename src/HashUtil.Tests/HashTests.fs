f770ffa0961d82f4dfa659a0c7d8c65f5320af85
Anu Bandi <ultimateanu@gmail.com>
2021-02-01 18:04:04 -0800

    added hashtype parsing tests

--- src/HashUtil.Tests/HashTests.fs
+++ src/HashUtil.Tests/HashTests.fs
@@ -2,6 +2,7 @@
 
 open HashUtil
 open HashUtil.Checksum
+open Microsoft.FSharp.Reflection
 open Xunit
 open Xunit.Abstractions
 
@@ -46,3 +47,28 @@ type ChecksumTests(output: ITestOutputHelper) =
         let hashAlg = Checksum.getHashAlgorithm hashType
         let strHash = computeHashOfString hashAlg inputStr
         Assert.Equal(expectedHash, strHash)
+
+    static member hashTypeStrings: obj [] seq =
+        Seq.ofList [ [| ["md5"; " md5   "; "MD5 "] ; Some MD5 |]
+                     [| ["sha1"; " Sha1   ";  "SHA1 "] ; Some SHA1 |]
+                     [| ["sha256"; " Sha256   "; "SHA256 "] ; Some SHA256 |]
+                     [| ["sha384"; " Sha384   "; "SHA384 "] ; Some SHA384 |]
+                     [| ["sha512"; " Sha512   "; "SHA512 "] ; Some SHA512 |]
+                     [| ["asha1"; " md6   "; "SHA513 "] ; None |]
+                    ]
+
+    [<Theory; MemberData("hashTypeStrings")>]
+    member _.``parse HashType``(inputStrs, expectedType) =
+        for inputStr in inputStrs do
+            let hashTypeMaybe = parseHashType inputStr
+            Assert.Equal(expectedType, hashTypeMaybe)
+
+    [<Fact>]
+    member _.``parse all possible types``() =
+        let parsedHashTypes =
+            typeof<HashType>
+                |> FSharpType.GetUnionCases
+                |> Array.map (fun info -> info.Name)
+                |> Array.map parseHashType
+
+        Assert.All(parsedHashTypes, (fun parsedHashType -> Assert.True(parsedHashType.IsSome)))
