3ea4a0d171316da059f922c8eb0230527092eb60
Anu Bandi <ultimateanu@gmail.com>
2021-02-05 16:33:28 -0800

    formatting tests

--- src/HashUtil.Tests/FsTests.fs
+++ src/HashUtil.Tests/FsTests.fs
@@ -19,8 +19,7 @@ type FsTempDirSetupFixture() =
 
     // CLEANUP
     interface IDisposable with
-        member this.Dispose() =
-            Directory.Delete(tempDir, true)
+        member this.Dispose() = Directory.Delete(tempDir, true)
 
     member _.TempDir = tempDir
 
@@ -182,44 +181,45 @@ type HashProperties(fsTempDirSetupFixture: FsTempDirSetupFixture, output: ITestO
 
 
 type FileHashes(fsTempDirSetupFixture: FsTempDirSetupFixture, output: ITestOutputHelper) =
-    let hiddenFilePath = Path.Combine(fsTempDirSetupFixture.TempDir, ".fakerc")
+    let hiddenFilePath =
+        Path.Combine(fsTempDirSetupFixture.TempDir, ".fakerc")
 
     // SETUP
     do
-        File.WriteAllText(hiddenFilePath, "config");
+        File.WriteAllText(hiddenFilePath, "config")
+
         if RuntimeInformation.IsOSPlatform(OSPlatform.Windows) then
-            File.SetAttributes(hiddenFilePath, FileAttributes.Hidden);
+            File.SetAttributes(hiddenFilePath, FileAttributes.Hidden)
 
     // CLEANUP
     interface IDisposable with
-        member this.Dispose() =
-            File.Delete(hiddenFilePath)
+        member this.Dispose() = File.Delete(hiddenFilePath)
 
     interface IClassFixture<FsTempDirSetupFixture>
 
     [<Fact>]
-    member _.``Hidden file (include)`` () =
+    member _.``Hidden file (include)``() =
         // Compute hash (including this hidden file)
         let includeHiddenFiles = true
+
         let fileHash =
             hiddenFilePath
-                |> makeHashStructure SHA256 includeHiddenFiles true
-                |> makeOption
+            |> makeHashStructure SHA256 includeHiddenFiles true
+            |> makeOption
 
         // Hash should exist and match
         Assert.True(fileHash.IsSome)
-        Assert.Equal(
-            "b79606fb3afea5bd1609ed40b622142f1c98125abcfe89a76a661b0e8e343910",
-            getHash fileHash.Value)
+        Assert.Equal("b79606fb3afea5bd1609ed40b622142f1c98125abcfe89a76a661b0e8e343910", getHash fileHash.Value)
 
     [<Fact>]
-    member _.``Hidden file (exclude)`` () =
+    member _.``Hidden file (exclude)``() =
         // Compute hash (excluding this hidden file)
         let includeHiddenFiles = false
+
         let fileHash =
             hiddenFilePath
-                |> makeHashStructure SHA256 includeHiddenFiles true
-                |> makeOption
+            |> makeHashStructure SHA256 includeHiddenFiles true
+            |> makeOption
 
         // Hash should exist and match
         Assert.True(fileHash.IsNone)
@@ -229,36 +229,40 @@ type DirHashes(fsTempDirSetupFixture: FsTempDirSetupFixture, output: ITestOutput
     interface IClassFixture<FsTempDirSetupFixture>
 
     [<Fact>]
-    member _.``Dir with 0 files (include empty dir)`` () =
+    member _.``Dir with 0 files (include empty dir)``() =
         // Setup dir with 0 files
-        let dirZero = Path.Combine(fsTempDirSetupFixture.TempDir, "dir_zero")
-        Directory.CreateDirectory(dirZero) |> ignore;
+        let dirZero =
+            Path.Combine(fsTempDirSetupFixture.TempDir, "dir_zero")
+
+        Directory.CreateDirectory(dirZero) |> ignore
 
         // Compute hash (including this empty dir)
         let includeEmptyDir = true
+
         let zeroFileDirHash =
             dirZero
-                |> makeHashStructure SHA256 false includeEmptyDir
-                |> makeOption
+            |> makeHashStructure SHA256 false includeEmptyDir
+            |> makeOption
 
         // Hash should exist and match
         Assert.True(zeroFileDirHash.IsSome)
-        Assert.Equal(
-            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-            getHash zeroFileDirHash.Value)
+        Assert.Equal("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", getHash zeroFileDirHash.Value)
 
     [<Fact>]
-    member _.``Dir with 0 files (exclude empty dir)`` () =
+    member _.``Dir with 0 files (exclude empty dir)``() =
         // Setup dir with 0 files
-        let dirZero = Path.Combine(fsTempDirSetupFixture.TempDir, "dir_zero")
-        Directory.CreateDirectory(dirZero) |> ignore;
+        let dirZero =
+            Path.Combine(fsTempDirSetupFixture.TempDir, "dir_zero")
+
+        Directory.CreateDirectory(dirZero) |> ignore
 
         // Compute hash (excluding this empty dir)
         let includeEmptyDir = false
+
         let zeroFileDirHash =
             dirZero
-                |> makeHashStructure SHA256 false includeEmptyDir
-                |> makeOption
+            |> makeHashStructure SHA256 false includeEmptyDir
+            |> makeOption
 
         // Hash should exist and match
         Assert.True(zeroFileDirHash.IsNone)
@@ -266,63 +270,64 @@ type DirHashes(fsTempDirSetupFixture: FsTempDirSetupFixture, output: ITestOutput
     [<Fact>]
     member _.``Dir with 1 file``() =
         // Setup dir with 1 file
-        let dirOne = Path.Combine(fsTempDirSetupFixture.TempDir, "dir_one")
-        Directory.CreateDirectory(dirOne) |> ignore;
-        File.WriteAllText(Path.Combine(dirOne, "file1.txt"), "1");
+        let dirOne =
+            Path.Combine(fsTempDirSetupFixture.TempDir, "dir_one")
+
+        Directory.CreateDirectory(dirOne) |> ignore
+        File.WriteAllText(Path.Combine(dirOne, "file1.txt"), "1")
 
         // Compute hash
         let oneFileDirHash =
             dirOne
-                |> makeHashStructure SHA256 false false
-                |> makeOption
+            |> makeHashStructure SHA256 false false
+            |> makeOption
 
         // Hash should exist and match
         Assert.True(oneFileDirHash.IsSome)
-        Assert.Equal(
-            "c0b9c17c8ac302513644256d06d1518a50c0c349e28023c2795a17dfa5479e1f",
-            getHash oneFileDirHash.Value)
+        Assert.Equal("c0b9c17c8ac302513644256d06d1518a50c0c349e28023c2795a17dfa5479e1f", getHash oneFileDirHash.Value)
 
     [<Fact>]
-    member _.``Dir with 2 files`` () =
+    member _.``Dir with 2 files``() =
         // Setup dir with 2 files
-        let dirTwo = Path.Combine(fsTempDirSetupFixture.TempDir, "dir_two")
-        Directory.CreateDirectory(dirTwo) |> ignore;
-        File.WriteAllText(Path.Combine(dirTwo, "file1.txt"), "1");
-        File.WriteAllText(Path.Combine(dirTwo, "file2.txt"), "2");
+        let dirTwo =
+            Path.Combine(fsTempDirSetupFixture.TempDir, "dir_two")
+
+        Directory.CreateDirectory(dirTwo) |> ignore
+        File.WriteAllText(Path.Combine(dirTwo, "file1.txt"), "1")
+        File.WriteAllText(Path.Combine(dirTwo, "file2.txt"), "2")
 
         // Compute hash
         let twoFileDirHash =
             dirTwo
-                |> makeHashStructure SHA256 false false
-                |> makeOption
+            |> makeHashStructure SHA256 false false
+            |> makeOption
 
         // Hash should exist and match
         Assert.True(twoFileDirHash.IsSome)
-        Assert.Equal(
-            "072d85c3b6926317ee8c340d4e989c9588c75408e63b5674571624a096faf9b5",
-            getHash twoFileDirHash.Value)
+        Assert.Equal("072d85c3b6926317ee8c340d4e989c9588c75408e63b5674571624a096faf9b5", getHash twoFileDirHash.Value)
 
 type FullTreeHash(fsTempDirSetupFixture: FsTempDirSetupFixture, output: ITestOutputHelper) =
-    let rootDir = Path.Combine(fsTempDirSetupFixture.TempDir, "root")
+    let rootDir =
+        Path.Combine(fsTempDirSetupFixture.TempDir, "root")
 
     // SETUP
     do
         // Root dir has a single file
-        Directory.CreateDirectory(rootDir) |> ignore;
-        File.WriteAllText(Path.Combine(rootDir, "shakespeare.txt"), "To be or not to be...");
+        Directory.CreateDirectory(rootDir) |> ignore
+        File.WriteAllText(Path.Combine(rootDir, "shakespeare.txt"), "To be or not to be...")
         // Dir A has sub dirs
         let dirA = Path.Combine(rootDir, "dir_a")
         let dirAInner = Path.Combine(dirA, "inner")
         Directory.CreateDirectory(dirA) |> ignore
         Directory.CreateDirectory(dirAInner) |> ignore
-        File.WriteAllText(Path.Combine(dirAInner, "inner_1.txt"), "inner file 1");
+        File.WriteAllText(Path.Combine(dirAInner, "inner_1.txt"), "inner file 1")
         // Dir B has multiple files
         let dirB = Path.Combine(rootDir, "dir_b")
         Directory.CreateDirectory(dirB) |> ignore
-        File.WriteAllText(Path.Combine(dirB, "file_1.txt"), "b file 1");
-        File.WriteAllText(Path.Combine(dirB, "file_2.txt"), "b file 2");
-        File.WriteAllText(Path.Combine(dirB, "image_a.txt"), "b image");
-        File.WriteAllText(Path.Combine(dirB, "song_a.txt"), "b song");
+        File.WriteAllText(Path.Combine(dirB, "file_1.txt"), "b file 1")
+        File.WriteAllText(Path.Combine(dirB, "file_2.txt"), "b file 2")
+        File.WriteAllText(Path.Combine(dirB, "image_a.txt"), "b image")
+        File.WriteAllText(Path.Combine(dirB, "song_a.txt"), "b song")
         // Dir C is empty
         let dirC = Path.Combine(rootDir, "dir_c")
         Directory.CreateDirectory(dirC) |> ignore
@@ -330,71 +335,74 @@ type FullTreeHash(fsTempDirSetupFixture: FsTempDirSetupFixture, output: ITestOut
         let dirD = Path.Combine(rootDir, "dir_d")
         Directory.CreateDirectory(dirD) |> ignore
         let hiddenFilePath1 = Path.Combine(dirD, ".config")
-        File.WriteAllText(hiddenFilePath1, "config stuff");
+        File.WriteAllText(hiddenFilePath1, "config stuff")
+
         if RuntimeInformation.IsOSPlatform(OSPlatform.Windows) then
-            File.SetAttributes(hiddenFilePath1, FileAttributes.Hidden);
+            File.SetAttributes(hiddenFilePath1, FileAttributes.Hidden)
+
         let hiddenFilePath2 = Path.Combine(dirD, ".vimrc")
-        File.WriteAllText(hiddenFilePath2, "vim stuff");
+        File.WriteAllText(hiddenFilePath2, "vim stuff")
+
         if RuntimeInformation.IsOSPlatform(OSPlatform.Windows) then
-            File.SetAttributes(hiddenFilePath2, FileAttributes.Hidden);
-        File.WriteAllText(Path.Combine(dirB, "info.txt"), "info");
+            File.SetAttributes(hiddenFilePath2, FileAttributes.Hidden)
+
+        File.WriteAllText(Path.Combine(dirB, "info.txt"), "info")
 
 
     // CLEANUP
     interface IDisposable with
-        member this.Dispose() =
-            Directory.Delete(rootDir, true)
+        member this.Dispose() = Directory.Delete(rootDir, true)
 
     interface IClassFixture<FsTempDirSetupFixture>
 
     [<Fact>]
-    member _.``Full tree hash (include hidden, include empty dir)`` () =
+    member _.``Full tree hash (include hidden, include empty dir)``() =
         let includeHiddenFiles = true
         let includeEmptyDir = true
+
         let rootHash =
             rootDir
-                |> makeHashStructure SHA256 includeHiddenFiles includeEmptyDir
-                |> makeOption
+            |> makeHashStructure SHA256 includeHiddenFiles includeEmptyDir
+            |> makeOption
+
         Assert.True(rootHash.IsSome)
-        Assert.Equal(
-            "a7bd95e3686bf708684575ad945e6ad999bbf71e811c108bdce1e39b6d6cd66f",
-            getHash rootHash.Value)
+        Assert.Equal("a7bd95e3686bf708684575ad945e6ad999bbf71e811c108bdce1e39b6d6cd66f", getHash rootHash.Value)
 
     [<Fact>]
-    member _.``Full tree hash (include hidden, exclude empty dir)`` () =
+    member _.``Full tree hash (include hidden, exclude empty dir)``() =
         let includeHiddenFiles = true
         let includeEmptyDir = false
+
         let rootHash =
             rootDir
-                |> makeHashStructure SHA256 includeHiddenFiles includeEmptyDir
-                |> makeOption
+            |> makeHashStructure SHA256 includeHiddenFiles includeEmptyDir
+            |> makeOption
+
         Assert.True(rootHash.IsSome)
-        Assert.Equal(
-            "316f1288632d00417e849505d8a70e3cf368fe4714e24bded2425655995fe601",
-            getHash rootHash.Value)
+        Assert.Equal("316f1288632d00417e849505d8a70e3cf368fe4714e24bded2425655995fe601", getHash rootHash.Value)
 
     [<Fact>]
-    member _.``Full tree hash (exclude hidden, include empty dir)`` () =
+    member _.``Full tree hash (exclude hidden, include empty dir)``() =
         let includeHiddenFiles = false
         let includeEmptyDir = true
+
         let rootHash =
             rootDir
-                |> makeHashStructure SHA256 includeHiddenFiles includeEmptyDir
-                |> makeOption
+            |> makeHashStructure SHA256 includeHiddenFiles includeEmptyDir
+            |> makeOption
+
         Assert.True(rootHash.IsSome)
-        Assert.Equal(
-            "82268ca33494bec2d17e1bcad8ac73744544183ef1d6b2c25b0988f88064f180",
-            getHash rootHash.Value)
+        Assert.Equal("82268ca33494bec2d17e1bcad8ac73744544183ef1d6b2c25b0988f88064f180", getHash rootHash.Value)
 
     [<Fact>]
-    member _.``Full tree hash (exclude hidden, exclude empty dir)`` () =
+    member _.``Full tree hash (exclude hidden, exclude empty dir)``() =
         let includeHiddenFiles = false
         let includeEmptyDir = false
+
         let rootHash =
             rootDir
-                |> makeHashStructure SHA256 includeHiddenFiles includeEmptyDir
-                |> makeOption
+            |> makeHashStructure SHA256 includeHiddenFiles includeEmptyDir
+            |> makeOption
+
         Assert.True(rootHash.IsSome)
-        Assert.Equal(
-            "1ae01db575f5965c003d47c026cb0bc141de0fb4897713a54a5296651ad743db",
-            getHash rootHash.Value)
+        Assert.Equal("1ae01db575f5965c003d47c026cb0bc141de0fb4897713a54a5296651ad743db", getHash rootHash.Value)
--- src/HashUtil.Tests/IOTests.fs
+++ src/HashUtil.Tests/IOTests.fs
@@ -5,10 +5,12 @@ open System.IO
 open Xunit
 open Xunit.Abstractions
 
-type DisplayTests(output:ITestOutputHelper) =
+type DisplayTests(output: ITestOutputHelper) =
     [<Fact>]
-    member _.``Print file hash`` () =
-        let hashStructure = ItemHash.File(path = "/path/to/file.txt", hash = "a1b2c3")
+    member _.``Print file hash``() =
+        let hashStructure =
+            ItemHash.File(path = "/path/to/file.txt", hash = "a1b2c3")
+
         let strWriter = new StringWriter()
 
         // Output without tree should be a single line.
@@ -22,13 +24,10 @@ type DisplayTests(output:ITestOutputHelper) =
         Assert.Equal(expectedStr, strWriter.ToString())
 
     [<Fact>]
-    member _.``Print dir tree hash (0 files)`` () =
+    member _.``Print dir tree hash (0 files)``() =
         let hashStructure =
-            ItemHash.Dir(
-                path = "/path/to/dir",
-                hash = "d1",
-                children = []
-            )
+            ItemHash.Dir(path = "/path/to/dir", hash = "d1", children = [])
+
         let strWriter = new StringWriter()
         printHashStructure hashStructure true strWriter
 
@@ -36,13 +35,14 @@ type DisplayTests(output:ITestOutputHelper) =
         Assert.Equal(expectedStr, strWriter.ToString())
 
     [<Fact>]
-    member _.``Print dir tree hash (1 file)`` () =
+    member _.``Print dir tree hash (1 file)``() =
         let hashStructure =
             ItemHash.Dir(
                 path = "/path/to/dir",
                 hash = "d1",
-                children = [ItemHash.File(path = "/path/to/dir/file1.txt", hash = "f1")]
+                children = [ ItemHash.File(path = "/path/to/dir/file1.txt", hash = "f1") ]
             )
+
         let strWriter = new StringWriter()
         printHashStructure hashStructure true strWriter
 
@@ -50,30 +50,36 @@ type DisplayTests(output:ITestOutputHelper) =
         Assert.Equal(expectedStr, strWriter.ToString())
 
     [<Fact>]
-    member _.``Print dir tree hash (2 files)`` () =
+    member _.``Print dir tree hash (2 files)``() =
         let hashStructure =
             ItemHash.Dir(
                 path = "/path/to/dir",
                 hash = "d1",
-                children = [ItemHash.File(path = "/path/to/dir/file1.txt", hash = "f1");
-                            ItemHash.File(path = "/path/to/dir/file2.txt", hash = "f2")]
+                children =
+                    [ ItemHash.File(path = "/path/to/dir/file1.txt", hash = "f1")
+                      ItemHash.File(path = "/path/to/dir/file2.txt", hash = "f2") ]
             )
+
         let strWriter = new StringWriter()
         printHashStructure hashStructure true strWriter
 
-        let expectedStr = "d1  /dir\n├── f1  file1.txt\n└── f2  file2.txt\n"
+        let expectedStr =
+            "d1  /dir\n├── f1  file1.txt\n└── f2  file2.txt\n"
+
         Assert.Equal(expectedStr, strWriter.ToString())
 
     [<Fact>]
-    member _.``Print dir multiple files no tree`` () =
+    member _.``Print dir multiple files no tree``() =
         let hashStructure =
             ItemHash.Dir(
                 path = "/path/to/dir",
                 hash = "d1",
-                children = [ItemHash.File(path = "/path/to/dir/file1.txt", hash = "f1");
-                            ItemHash.File(path = "/path/to/dir/file2.txt", hash = "f2");
-                            ItemHash.File(path = "/path/to/dir/file3.txt", hash = "f3")]
+                children =
+                    [ ItemHash.File(path = "/path/to/dir/file1.txt", hash = "f1")
+                      ItemHash.File(path = "/path/to/dir/file2.txt", hash = "f2")
+                      ItemHash.File(path = "/path/to/dir/file3.txt", hash = "f3") ]
             )
+
         let strWriter = new StringWriter()
         printHashStructure hashStructure false strWriter
 
