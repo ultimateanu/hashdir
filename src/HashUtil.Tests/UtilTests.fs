b456941f1b330d6a6e17d787025293b0edc5b5e3
Anu Bandi <ultimateanu@gmail.com>
2021-02-27 14:30:20 -0800

    adding util tests

---
+++ src/HashUtil.Tests/UtilTests.fs
@@ -0,0 +1,43 @@
+﻿module UtilTests
+
+open HashUtil
+open HashUtil.Checksum
+open HashUtil.Util
+open Xunit
+open Xunit.Abstractions
+open System
+
+type UtilTests(output: ITestOutputHelper) =
+    [<Theory>]
+    [<InlineData("The ", "the ")>]
+    [<InlineData("Bruce Wayne", "bruce wayne")>]
+    member _.``toStrLower simple strings``(input, expectedOutput) =
+        Assert.Equal(expectedOutput, toStrLower input)
+
+    [<Fact>]
+    member _.``toStrLower discriminated union types``() =
+        Assert.Equal("md5", toStrLower MD5)
+        Assert.Equal("sha1", toStrLower SHA1)
+        Assert.Equal("sha384", toStrLower SHA384)
+
+    [<Fact>]
+    member _.``makeOption from error``() =
+        Assert.Equal(None, Util.makeOption (Error "something went wrong"))
+        Assert.Equal(None, Util.makeOption (Error 1))
+
+    [<Fact>]
+    member _.``makeOption from success result``() =
+        Assert.Equal(Some "data", Util.makeOption (Ok "data"))
+        Assert.Equal(Some 1, Util.makeOption (Ok 1))
+
+    [<Fact>]
+    member _.``printColor``() =
+        let oldOut = Console.Out
+        use out = new IO.StringWriter()
+        Console.SetOut(out)
+        Util.printColor ConsoleColor.Green "MATCHES"
+        Console.SetOut(oldOut)
+        let printOutput = out.GetStringBuilder().ToString()
+
+        Assert.Equal("MATCHES", printOutput)
+        Assert.Equal(ConsoleColor.Gray, Console.ForegroundColor)
--- src/HashUtil/Util.fs
+++ src/HashUtil/Util.fs
@@ -7,14 +7,15 @@ module Util =
     let toStrLower x =
         x.ToString().ToLower()
 
-    let printColor color str =
-        Console.ForegroundColor <- color
-        printf "%s" str
-        Console.ResetColor()
-
-    let getDirName path = DirectoryInfo(path).Name
-
     let makeOption x =
         match x with
         | Error _ -> None
         | Ok v -> Some(v)
+
+    let getDirName path =
+        DirectoryInfo(path).Name
+
+    let printColor color str =
+        Console.ForegroundColor <- color
+        printf "%s" str
+        Console.ResetColor()
