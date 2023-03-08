0d109239fcb4c3301529fdc322c1c0db103d184d
Anu Bandi <ultimateanu@gmail.com>
2023-03-07 19:52:34 -0800

    updating release script

--- src/make_release.fsx
+++ src/make_release.fsx
@@ -1,6 +1,7 @@
 #!/usr/bin/env dotnet fsi
 
 #r "System.IO.Compression.FileSystem.dll"
+#r "System.Security.Cryptography.dll"
 #load "HashUtil/Util.fs"
 
 open System
@@ -137,7 +138,7 @@ let buildSingleBinary (profile: PublishSpec) =
         sprintf "%s_%s" nameAndVersion profile.Name
 
     let oldProfileDir =
-        "src/App/bin/Release/net6.0/publish/binary"
+        "src/App/bin/Release/net7.0/publish/binary"
 
     let newProfileDir = Path.Combine(releaseDir, releaseName)
 
@@ -166,7 +167,7 @@ let makeNuGetRelease () =
 let makeDotnetRelease () =
     dotnet "publish -c Release -p:PublishProfile=dotnet src/App/App.fsproj"
     let releaseName = sprintf "%s_dotnet" nameAndVersion
-    Directory.Move("src/App/bin/Release/net6.0/publish/dotnet", Path.Combine(releaseDir, releaseName))
+    Directory.Move("src/App/bin/Release/net7.0/publish/dotnet", Path.Combine(releaseDir, releaseName))
     compressDir Zip releaseName
     compressDir TarGz releaseName
 
