ba1f9d1684c54c435c012edb28fe4a05da518b08
Anu Bandi <ultimateanu@gmail.com>
2024-05-25 15:13:08 -0700

    bumping version to 1.3.2 for net8.0

--- src/make_release.fsx
+++ src/make_release.fsx
@@ -13,7 +13,7 @@ open System.Security.Cryptography
 open HashUtil.Util
 
 // Configuration ----------------------------------------------------
-let versionStr = "1.3.1"
+let versionStr = "1.3.2"
 // ------------------------------------------------------------------
 
 let releaseDir = "release"
